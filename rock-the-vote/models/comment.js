75081ddbdfa2c6dfb5cfe470d5ae5220009f83c7
isaabutaa <isaabutaa@gmail.com>
2020-03-20 16:02:54 -0400

    updated the comment model

--- rock-the-vote/models/comment.js
+++ rock-the-vote/models/comment.js
@@ -5,6 +5,16 @@ const commentSchema = new Schema({
     comment: {
         type: String,
         required: true
+    },
+    issue: {
+        type: Schema.Types.ObjectId,
+        ref: "Issue",
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     }
 })
 
